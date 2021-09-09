f3c400db8bd04aa38f522075e2b7e38812edbcad
Mohanad Maarouf <mohanadcodes@gmail.com>
2021-09-09 22:13:49 +0100

    Changed the trainer.py and the getdata.py to convert the preprocessing into a pipeline and move it into trainer.py

--- dreamteam/trainer.py
+++ dreamteam/trainer.py
@@ -1,11 +1,13 @@
 import joblib
-from getdata import get_data, clean_data, preproc_gen, preproc_x, preproc_y
+from getdata import get_data, clean_data, preproc_gen, preproc_y
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, RobustScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+
 
 from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score
 
@@ -30,25 +32,87 @@ class Trainer(object):
 
 
     def set_pipeline(self):
+        "set numerical features"
+
+        numeric_features = [
+            'hdl', 'ldl', 'total_cholesterol', 'triglycerides', 'weightkg',
+            'hipgirthm', 'neckgirthm', 'waistgirthm', 'waisthip', 'sitsysm',
+            'sitdiam', 'packs_week', 'pack_years', 'naps', 'snore_freq',
+            'num_pregnancies', 'age', 'heightcm', 'caffeine', 'alcohol_wk',
+            'eval_general', 'eval_life', 'eval_health', 'snore_vol', 'choke_freq',
+            'apnea_freq', 'awake_freq', 'cups_coffee'
+        ]
+
+        "set categorical features as features which aren't numerical or target"
+
+        categorical_features = ['sex', 'race', 'education_survey1',
+            'nasal_cong_none', 'any_cvd', 'hypertension_ynd', 'stroke_ynd',
+            'asthma_ynd', 'thyroid_ynd', 'thyroid_problem',
+            'arthritis_ynd', 'emphysema_ynd', 'menopausal_status',
+            'asthma_med', 'cholesterol_med', 'depression_med',
+            'htn_med', 'decongestants_med', 'antihistamines_med',
+            'anxiety_med', 'diabetes_med',  'sedative_med',
+            'thyroid_med'
+        ]
+        # 'diabetes_ynd',
+        # 'apnea',
+        #'narcotics_med'
+        # 'androgen_med'
+        # 'stimulants_med'
+
+        "categorical transformer"
+
+        categorical_transformer = Pipeline(
+            steps=[('imputer', SimpleImputer(strategy='most_frequent'))])
+
+        "categorical transformer 2"
+        ohe_features = ['hormone_therapy']
+
+        ohe_transformer = Pipeline(
+            steps=[('imputer_2', SimpleImputer(strategy='constant', fill_value='Unknown')),
+                ('ohe', OneHotEncoder(handle_unknown='ignore', sparse=False))]
+        )
+
+        "numerical transformer"
+
+        numeric_transformer = Pipeline(
+            steps=[('imputer',
+                    SimpleImputer(strategy='mean')), ('scaler', RobustScaler())])
+
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('num', numeric_transformer, numeric_features),
+                ('cat', categorical_transformer, categorical_features),
+                ('ohe_transform', ohe_transformer, ohe_features)], remainder='drop')
+
+        prepipe = Pipeline(steps=[('preprocessor', preprocessor)])
+        
         """defines the pipeline as a class attribute"""
         #### Scaling numerical features - categorical features and targets removed from this listt
         self.pipeline = Pipeline([
+            ('preproc_pipeline', prepipe),
             ('r_forest_classifier', RandomForestClassifier())
         ])
 
     def run(self):
         print("Running Models")
         self.set_pipeline()
-        #self.mlflow_log_param("model", "Linear")
+        
+        # TST Pipeline
         self.pipeline.fit(self.X_train, self.y1_train)
         self.evaluate(self.X_test, self.y1_test)
-        self.save_model_locally('tst')
+        self.save_model_locally('tst_pipeline')
+        
+        # WASO Pipeline
         self.pipeline.fit(self.X_train, self.y2_train)
         self.evaluate(self.X_test, self.y2_test)
-        self.save_model_locally('waso')
+        self.save_model_locally('waso_pipeline')
+
+        #SE PIPELINE
         self.pipeline.fit(self.X_train, self.y3_train)
         self.evaluate(self.X_test, self.y3_test)
-        self.save_model_locally('se')
+        self.save_model_locally('se_pipeline')
+
         print("Successfully ran all models")
 
     def evaluate(self, X_test, y_test):
@@ -73,13 +137,16 @@ class Trainer(object):
 
 if __name__ == "__main__":
     df, data_df = get_data()
+    #print(df.shape, data_df.shape)
 
     data_df = clean_data(df, data_df, 90, duplicates = True)
     
-    data_df, targs = preproc_gen(data_df)
-
-    X = preproc_x(data_df, targs, 0.975)
     y = preproc_y(data_df, 50, 85, 95, 360, 410)
+    
+    data_df, targs = preproc_gen(data_df)
+    
+    X = data_df
+    #X = preproc_x(data_df, targs, 0.975)
 
     # Train and save model locally
     trainer = Trainer(X=X, y=y)
